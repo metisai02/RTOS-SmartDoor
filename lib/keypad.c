1f129aa1d43a830b9ef07135da41016f8d0959ac
nga <nga.leddt02@hcmut.edu.vn>
2023-04-30 02:17:39 +0700

    Keypad

--- lib/keypad.c
+++ lib/keypad.c
@@ -4,5 +4,43 @@
  *  Created on: Mar 6, 2023
  *      Author: DELL
  */
+#include "keypad.h"
+#include "FreeRTOS.h"
+#include "task.h"
+static char keyPad[4][4] = {{'1', '2', '3', 'A'},
+                            {'4', '5', '6', 'B'},
+                            {'7', '8', '9', 'C'},
+                            {'*', '0', '#', 'D'}};
 
+uint8_t tableColum[4] = {COL_PIN_1, COL_PIN_2, COL_PIN_3, COL_PIN_4};
+uint8_t tableRow[4] = {ROW_PIN_1, ROW_PIN_2, ROW_PIN_3, ROW_PIN_4};
+
+void keypad_init(void)
+{
+    for (uint8_t colum = 0; colum < 4; colum++)
+    {
+        HAL_GPIO_WritePin(KEYPAD_PORT, tableColum[colum], GPIO_PIN_SET);
+    }
+}
+
+char get_char(void)
+{
+    for (uint8_t colum = 0; colum < 4; colum++)
+    {
+        HAL_GPIO_WritePin(KEYPAD_PORT, tableColum[colum], GPIO_PIN_RESET);
+        for (uint8_t row = 0; row < 4; row++)
+        {
+            if (!HAL_GPIO_ReadPin(KEYPAD_PORT, tableRow[row]))
+                {
+                    vTaskDelay(100);
+                    while (!HAL_GPIO_ReadPin(KEYPAD_PORT, tableRow[row]))
+                    {
+                    };
+                    return keyPad[row][colum];
+            }
+        }
+        HAL_GPIO_WritePin(KEYPAD_PORT, tableColum[colum], GPIO_PIN_SET);
+    }
+    return 0;
+}
 
--- lib/keypad.h
+++ lib/keypad.h
@@ -7,6 +7,22 @@
 
 #ifndef KEYPAD_H_
 #define KEYPAD_H_
+#include "main.h"
+
+#define KEYPAD_PORT         GPIOA
+
+#define ROW_PIN_1           GPIO_PIN_4
+#define ROW_PIN_2           GPIO_PIN_5
+#define ROW_PIN_3           GPIO_PIN_6
+#define ROW_PIN_4           GPIO_PIN_7
+
+#define COL_PIN_1           GPIO_PIN_0
+#define COL_PIN_2           GPIO_PIN_1
+#define COL_PIN_3           GPIO_PIN_2
+#define COL_PIN_4           GPIO_PIN_3
+
+void keypad_init(void);
+char get_char(void);
 
 
 
